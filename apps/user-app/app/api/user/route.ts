8935a0daeef083d9a4e33a5b80c02444a7cf3c27
muzaffar-angularminds <muzaffar@angularminds.in>
2024-09-30 15:25:56 +0530

    Checkpoint Reached

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -1,16 +1,17 @@
-import  PrismaClient  from '@repo/db/client';
-import { NextResponse } from "next/server"
-
-const client =  PrismaClient;
+import { getServerSession } from "next-auth"
+import { NextResponse } from "next/server";
+import { authOptions } from "../../lib/auth";
 
 export const GET = async () => {
-    await client.user.create({
-        data: {
-            email: "asd23@gmail.com",
-            name: "adsads"
-        }
-    })
+    const session = await getServerSession(authOptions);
+    if (session.user) {
+        return NextResponse.json({
+            user: session.user
+        })
+    }
     return NextResponse.json({
-        message: "hi there"
+        message: "You are not logged in"
+    }, {
+        status: 403
     })
 }
