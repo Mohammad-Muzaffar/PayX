f3d60f0b2725360f40316f0f14d7b6f5208ee167
muzaffar-angularminds <muzaffar@angularminds.in>
2024-10-03 12:33:45 +0530

    Made Transfer page

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Providers } from "../providers/provider";
+import { AppbarClient } from "../components/AppbarClient";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,11 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Providers>
-          {children}
-        </Providers>
-      </body>
+        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          <Providers>
+            <AppbarClient />
+            {children}
+          </Providers>
+        </body>
     </html>
   );
 }
