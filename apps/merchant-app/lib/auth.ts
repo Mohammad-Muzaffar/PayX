c6caed9e8a3936fcbb10a4ba2eda0d7b68b97808
muzaffar-angularminds <muzaffar@angularminds.in>
2024-10-03 10:26:04 +0530

    #4 patched some bugs

--- apps/merchant-app/lib/auth.ts
+++ apps/merchant-app/lib/auth.ts
@@ -3,10 +3,6 @@ import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions = {
     providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-        }),
         GitHubProvider({
             clientId: process.env.GITHUB_ID || "",
             clientSecret: process.env.GITHUB_SECRET || ""
--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -55,10 +55,6 @@ export const authOptions = {
             return null
           },
         }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-        }),
         GitHubProvider({
             clientId: process.env.GITHUB_ID || "",
             clientSecret: process.env.GITHUB_SECRET || ""
